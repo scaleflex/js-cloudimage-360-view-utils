da4bb5b5015ffa6d15d81c8c90f71684733fb91b
Amr Wagdy <wagdy@scaleflex.com>
2022-01-17 17:32:48 +0200

    improvement: hotspots positions

--- src/utils/hotspot/update-hotspot-icon-position.js
+++ src/utils/hotspot/update-hotspot-icon-position.js
@@ -1,14 +1,14 @@
 export const updateHotspotIconPosition = (container, initialDimensions, icon, xCoord, yCoord) => {
   icon.style.visibility = 'visible';
   icon.style.opacity = 1;
-  icon.style.left = 0;
-  icon.style.top = 0;
+  icon.style.left = -icon.offsetWidth / 2;
+  icon.style.top = -icon.offsetHeight / 2;
 
   const positionXRatio = container.offsetWidth / initialDimensions[0];
   const positionYRatio = container.offsetHeight / initialDimensions[1];
 
-  const translateX = `${(positionXRatio * xCoord) - icon.offsetWidth / 2}px`;
-  const translateY = `${(positionYRatio * yCoord) - icon.offsetHeight / 2}px`;
+  const translateX = `${(positionXRatio * xCoord)}px`;
+  const translateY = `${(positionYRatio * yCoord)}px`;
 
   icon.style.transform = `translate3d(${translateX}, ${translateY}, 0)`;
 };
