fc9e9d6cbd8185d12339779c07f25bd12a0460bf
Amr Wagdy <wagdy@scaleflex.com>
2022-01-24 10:52:43 +0200

    improvement: folder tree

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ export { preloadOriginalImages } from './utils/load-images/preload-original-imag
 export { contain } from './utils/responsive/contain';
 export { getCurrentOriginalImage } from './utils/magnify/get-current-original-image';
 export { magnify } from './utils/magnify/magnify';
-export { getSpeedFactor } from './utils/auto-play/getSpeedFactor';
+export { getSpeedFactor } from './utils/auto-play/get-speed-factor';
 export { isCompletedOneCycle } from './utils/auto-play/is-completed-one-cycle';
 export { getContainerResponsiveWidth } from './utils/responsive/get-container-responsive-width';
 export { getContainerResponsiveHeight } from './utils/responsive/get-container-responsive-height';
 export { getMovingDirection } from './utils/spin-y/get-moving-direction';
-export { initControls } from './utils/controls/initControls';
+export { initControls } from './utils/controls/init-controls';
 export { addClass } from './utils/common/add-class';
 export { removeClass } from './utils/common/remove-class';
 export { getItemSkipped } from './utils/controls/get-item-skipped';
